cd43af0ab65ae5b12bc54908f547318633971193
Shorif <sashipon001@gmail.com>
2022-05-13 23:59:08 +0600

    update

--- src/app/view-contact/view-contact.component.ts
+++ src/app/view-contact/view-contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MyContact } from '../models/myContact';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-view-contact',
@@ -9,15 +11,32 @@ import { ActivatedRoute } from '@angular/router';
 export class ViewContactComponent implements OnInit {
 
   contactId:string | null = null;
+  loading:boolean = false;
+  contact:MyContact = {} as MyContact;
+  errorMessage:string | null = null;
 
-  constructor(private activatedRoute:ActivatedRoute) { }
+  constructor(private activatedRoute:ActivatedRoute, private contService:ContactService) { }
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe((param)=>{
       console.log(param);
       this.contactId = param.get('contactId');
     })
-
+    if(this.contactId){
+      this.loading = true;
+      this.contService.getContacts(this.contactId).subscribe((data:MyContact)=>{
+        this.contact = data;
+        this.loading = false;
+      }, (error)=>{
+        this.errorMessage = error;
+        this.loading = false;
+      })
+    }
   }
 
+  isNotEmpty(){
+    return Object.keys(this.contact).length >0;
+  }
+
+  
 }
